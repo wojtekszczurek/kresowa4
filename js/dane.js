3860b88e95b5ca8cea68c674897f0582086005f0
joo14ppp <piotr.szczurek64@gmail.com>
2019-10-02 00:12:25 +0200

    Dodanie danych js

---
+++ js/dane.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var dane=[];
+dane=[
+    {rok:"2019",miesiac:"maj",czynsz:"700",woda:"1886",gaz:"287"},
+    {rok:"2019",miesiac:"czerwiec",czynsz:"700",woda:"1891",gaz:"292"},
+    {rok:"2019",miesiac:"lipiec",czynsz:"700",woda:"1899",gaz:"300"}
+]
+
+
